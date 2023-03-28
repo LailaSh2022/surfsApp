a25c6561ab5189ad0ae96f2f9ab7607cb5281dd1
Sophon Keo <54705746+keosophon@users.noreply.github.com>
2023-03-29 09:29:19 +1300

    add blank home

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
 // screens
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./screens/Home";
 import Login from "./screens/Login";
 
+
+const Stack = createStackNavigator();
+
 export default function App() {
-  return <Login />;
+    return (
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{ title: "SurfsApp Limited" }}
+            />
+            <Stack.Screen name="Login" component={Login} />
+          </Stack.Navigator>
+        </NavigationContainer>
+     )
+    ;
 }
