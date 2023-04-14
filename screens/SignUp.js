e5db543714c46601bccdaa8e610c3571f3f16718
Tatiane Da Silva <764702131@nzse.ac.nz>
2023-04-14 16:23:06 +1200

    Tested sign up page with database

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { Text, View, ScrollView, Alert } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 //Using formik
 import { Formik } from "formik";
 //Icons
 import { Octicons, Ionicons } from "@expo/vector-icons";
-//import PageFooter from "../components/PageFooter";
+import PageFooter from "../components/PageFooter";
 // Styles
 import {
   StyledContainer,
@@ -30,158 +30,70 @@ import {
   MediumPageLogo,
 } from "./../components/Styles";
 
-import { CheckUserNameExists, SignUpNewUser } from "../Database";
-
 const { brand, darkLight, tertiary } = Colors;
-const initialValues = {
-  UserName: "",
-  FirstName: "",
-  LastName: "",
-  DateOfBirth: "",
-  Email: "",
-  Phone_Number: "",
-  Password: "",
-  ConfirmPassword: "",
-};
-const onSubmit = (values) => {
-  if (values.UserName == "") {
-    Alert.alert("Error", "UserName cannot be empty");
-    return;
-  }
-  CheckUserNameExists(values.UserName)
-    .then((exists) => {
-      if (exists) {
-        Alert.alert("Error", "Username exists. Please choose another one!");
-        return;
-      }
-    })
-    .catch((error) => {
-      console.log(`Error while checking user: ${error}`);
-      return;
-    });
-
-  if (values.FirstName == "") {
-    Alert.alert("Error", "FirstName cannot be empty");
-    return;
-  }
-
-  if (values.LastName == "") {
-    Alert.alert("Error", "LastName cannot be empty");
-    return;
-  }
-
-  if (values.DateOfBirth == "") {
-    Alert.alert("Error", "DateOfBirth cannot be empty");
-    return;
-  }
-
-  if (values.Email == "") {
-    Alert.alert("Error", "Email cannot be empty");
-    return;
-  }
-
-  if (values.Phone_Number == "") {
-    Alert.alert("Error", "Mobile Number cannot be empty");
-    return;
-  }
-
-  if (values.Password == "") {
-    Alert.alert("Error", "Password cannot be empty");
-    return;
-  }
-
-  if (values.Password != values.ConfirmPassword) {
-    Alert.alert("Error", "Password and Confirm Password must be the same");
-    return;
-  }
-  SignUpNewUser(values);
-};
-
 const SignUp = () => {
   const [hidePassword, setHidePassword] = useState(true);
   return (
     <StyledContainer>
       <ScrollView>
         <StatusBar style="dark" />
-
+          <ExtraView>
+            
+            <ExtraText>Enter your details below </ExtraText>
+          </ExtraView>
         <InnerContainer>
-          <View style={{ position: "absolute", left: "1%", top: "0.5%" }}>
-            <UserImage
-              source={require("./../assets/UserImage.png")}
-              style={{
-                //position: "absolute",
-                marginTop: 1,
-              }}
-            />
-          </View>
-          <MediumPageLogo
-            resizeMode="cover"
-            source={require("./../assets/Logo.png")}
-            style={{
-              left: "4%",
-              top: "3%",
-              // position: "absolute",
-            }}
-          />
 
-          <Formik initialValues={initialValues} onSubmit={onSubmit}>
+          <Formik
+            initialValues={{
+              username: "",
+              email: "",
+              name: "",
+              MobileNum: "",       
+              password: "",
+              confirmPassword: "",
+              
+            }}
+            onSubmit={(values) => console.log(values)}
+          >
             {({ handleChange, handleBlur, handleSubmit, values }) => (
               <StyledFormArea>
                 <MyTextInput
-                  placeholder="UserName"
+                  placeholder="Username"
                   placeholderTextColor={darkLight}
-                  onChangeText={handleChange("UserName")}
-                  onBlur={handleBlur("UserName")}
-                  values={values.UserName}
-                  //keyboardType="email-address"
+                  onChangeText={handleChange("username")}
+                  onBlur={handleBlur("username")}
+                  values={values.username}
+                  keyboardType="email-address"
                 />
-                <MyTextInput
-                  placeholder="FirstName"
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("FirstName")}
-                  onBlur={handleBlur("FirstName")}
-                  values={values.FirstName}
-                />
-
-                <MyTextInput
-                  placeholder="LastName"
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("LastName")}
-                  onBlur={handleBlur("LastName")}
-                  values={values.LastName}
-                />
-
-                <MyTextInput
-                  placeholder="Date of Birth"
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("DateOfBirth")}
-                  onBlur={handleBlur("DateOfBirth")}
-                  values={values.DateOfBirth}
-                />
-
                 <MyTextInput
                   placeholder="Email Address"
                   placeholderTextColor={darkLight}
-                  onChangeText={handleChange("Email")}
-                  onBlur={handleBlur("Email")}
-                  values={values.Email}
+                  onChangeText={handleChange("email")}
+                  onBlur={handleBlur("email")}
+                  values={values.email}
                   keyboardType="email-address"
                 />
-
+                <MyTextInput
+                  placeholder="Name"
+                  placeholderTextColor={darkLight}
+                  onChangeText={handleChange("name")}
+                  onBlur={handleBlur("name")}
+                  values={values.name}
+                />     
                 <MyTextInput
                   placeholder="Mobile Number"
                   placeholderTextColor={darkLight}
-                  onChangeText={handleChange("Phone_Number")}
-                  onBlur={handleBlur("Phone_Number")}
-                  values={values.Phone_Number}
-                  //keyboardType="email-address"
+                  onChangeText={handleChange("MobileNum")}
+                  onBlur={handleBlur("MobileNum")}
+                  values={values.MobileNum}
+                  keyboardType="email-address"
                 />
                 <MyTextInput
                   placeholder="Password"
                   placeholderTextColor={darkLight}
-                  onChangeText={handleChange("Password")}
-                  onBlur={handleBlur("Password")}
-                  values={values.Password}
+                  onChangeText={handleChange("password")}
+                  onBlur={handleBlur("password")}
+                  values={values.password}
                   secureTextEntry={hidePassword}
                   isPassword={true}
                   hidePassword={hidePassword}
@@ -190,15 +102,16 @@ const SignUp = () => {
                 <MyTextInput
                   placeholder="Confirm Password"
                   placeholderTextColor={darkLight}
-                  onChangeText={handleChange("ConfirmPassword")}
-                  onBlur={handleBlur("ConfirmPassword")}
-                  values={values.ConfirmPassword}
+                  onChangeText={handleChange("confirmPassword")}
+                  onBlur={handleBlur("confirmPassword")}
+                  values={values.confirmPassword}
                   secureTextEntry={hidePassword}
                   isPassword={true}
                   hidePassword={hidePassword}
                   setHidePassword={setHidePassword}
                 />
 
+                
                 <StyledButton onPress={handleSubmit}>
                   <ButtonText>Sign Up</ButtonText>
                 </StyledButton>
@@ -206,24 +119,30 @@ const SignUp = () => {
             )}
           </Formik>
           <ExtraView>
-            <ExtraText>Already have an account? </ExtraText>
-            <TextLink>
-              <TextLinkContent>Sign In</TextLinkContent>
-            </TextLink>
-          </ExtraView>
+          <ExtraText>Already have an account? </ExtraText>
+          <TextLink>
+            <TextLinkContent>Sign In</TextLinkContent>
+          </TextLink>
+        </ExtraView>
           <ExtraView>
             <ExtraText>By signing up you agree to our </ExtraText>
-            <TextLink>
-              <TextLinkContent>Terms of Use</TextLinkContent>
-            </TextLink>
+          </ExtraView>
+            <ExtraView>
+              <TextLinkContent>Terms of Use </TextLinkContent>
             <ExtraText>and </ExtraText>
             <TextLink>
               <TextLinkContent>Privacy Policy</TextLinkContent>
             </TextLink>
           </ExtraView>
+
         </InnerContainer>
       </ScrollView>
-      <PageFooter />
+      <View style={{ flexDirection: "column", height: "17%" }} />
+      <View>
+        <PageFooter />
+      </View>
+
+      
     </StyledContainer>
   );
 };
