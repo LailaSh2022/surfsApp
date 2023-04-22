4e87bb1c04e4208fc38c097176e6f10325783688
Sophon Keo <54705746+keosophon@users.noreply.github.com>
2023-04-23 11:04:13 +1200

    ..

--- screens/__tests__/OrderSummary.test.js
+++ screens/__tests__/OrderSummary.test.js
@@ -4,21 +4,25 @@ import "jest-styled-components";
 
 import OrderSummary from "../OrderSummary";
 
-it("Testing OrderSummary screen against its snapshot. If there is unexpected change in UI, it will prompt test failed.", () => {
-  const mockRoute = {
-    params: {
-      OrderSummary: {
-        OrderId: 1,
-        Date: "16/04/2023",
-        Rate: 3.05,
-        From_Currency: "NZD",
-        To_Currency: "BRL",
-        Amount: 350,
-        Fee: 0.01,
+it(
+  "Testing OrderSummary screen against its snapshot. " +
+    "If there is unexpected change in UI, it will prompt test failed.",
+  () => {
+    const mockRoute = {
+      params: {
+        OrderSummary: {
+          OrderId: 1,
+          Date: "16/04/2023",
+          Rate: 3.05,
+          From_Currency: "NZD",
+          To_Currency: "BRL",
+          Amount: 350,
+          Fee: 0.01,
+        },
       },
-    },
-  };
+    };
 
-  const tree = renderer.create(<OrderSummary route={mockRoute} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+    const tree = renderer.create(<OrderSummary route={mockRoute} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  }
+);
